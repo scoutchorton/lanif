a6a82a0f00fc706b3e4c92cec178e3c80cacb46b
scoutchorton <scoutchorton@yahoo.com>
2020-11-18 12:57:19 -0500

    Finished Term panel

--- io/scoutchorton/lanif/components/Term.java
+++ io/scoutchorton/lanif/components/Term.java
@@ -5,13 +5,21 @@ import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
 public class Term extends JPanel {
+	//Exponent
 	protected int exponent;
-	//protected JSpinner.NumberEditor exponentArea;
 	protected JSpinner exponentArea;
+
+	//Coefficient
 	protected int coefficient;
-	//protected JSpinner.NumberEditor coefficientArea;
 	protected JSpinner coefficientArea;
+
+	//Variable
 	protected String variable;
 	protected JLabel variableArea;
 
@@ -19,6 +27,9 @@ public class Term extends JPanel {
 		this("x");
 	}
 	public Term(String var) {
+		//Initalize panel utilizing the GridBag layout 
+		super(new GridBagLayout());
+
 		//Initalize model variables
 		this.exponent = 1;
 		this.coefficient = 1;
@@ -30,10 +41,21 @@ public class Term extends JPanel {
 		SpinnerNumberModel coefficientNumberModel = new SpinnerNumberModel(this.coefficient, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
 		this.coefficientArea = new JSpinner(coefficientNumberModel);
 		this.variableArea = new JLabel(this.variable);
+		GridBagConstraints c = new GridBagConstraints();
 		
+		//Set half height font
+		Font exponentFont = exponentArea.getFont();
+		Font newExponentFont = new Font(exponentFont.getName(), exponentFont.getStyle(), (int)(exponentFont.getSize() * 0.75));
+		exponentArea.setFont(newExponentFont);
+
 		//Add components to JPanel
-		this.add(this.exponentArea);
-		this.add(this.variableArea);
-		this.add(this.coefficientArea);
+		this.add(this.coefficientArea, c);
+		c.insets = new Insets(0, 10, 0, 5);
+		c.fill = GridBagConstraints.BOTH;
+		this.add(this.variableArea, c);
+		c.insets = new Insets(0, 0, 0, 0);
+		c.fill = GridBagConstraints.NONE;
+		c.anchor = GridBagConstraints.NORTH;
+		this.add(this.exponentArea, c);
 	}
 }
